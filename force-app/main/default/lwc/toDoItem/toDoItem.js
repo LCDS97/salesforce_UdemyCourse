994980f828fa1a4958bd03a29ffcef830a2ff4c5
lcds97 <lucas.cds1997@gmail.com>
2022-08-13 04:20:08 -0300

    Adicionando CSS nas tarefas - toDoItem

--- force-app/main/default/lwc/toDoItem/toDoItem.js
+++ force-app/main/default/lwc/toDoItem/toDoItem.js
@@ -5,4 +5,10 @@ export default class ToDoItem extends LightningElement {
     @api tarefaNome;
     @api done = false;
 
+
+    // Verificando pela variavel booleane done, se a tarefa é próxima tarefa ou completa, ele ira aplicar a classe especifica para capturar o valor no CSS
+    get verificarTarefa(){
+        return this.done ? "tarefa completa" : "proxima tarefa"
+    }
+
 }
